28b6e75b987390a82bedc11120948c8f1c4745e3
nbayoungnate4kt <nathanael134@hotmail.com>
2019-03-28 17:12:34 -0300

    image site

--- client/main.js
+++ client/main.js
@@ -8,22 +8,68 @@ Template.myJumbo.events({
 	'click .js-addImg'(event){
 		$("#addImgModal").modal("show");
 	}
-})
+});
 
 Template.addImg.events({
-	'click .js-saveImg'(event, instance){
+	'click .js-saveImg'(event){
 		var imgTitle = $("#imgTitle").val();
 		var imgPath = $("#imgPath").val();
 		var imgDesc = $("#imgDesc").val();
-
+		
 		$("#imgTitle").val('');
 		$("#imgPath").val('');
 		$("#imgDesc").val('');
+		$("#addImgPreview").attr('src','nba.jpg');
+		$("#addImgModal").modal("hide");
+		imagesDB.insert({"title":imgTitle, "path":imgPath, "desc":imgDesc, "createdOn":Date()});
+	},
+	'click .js-cancelAdd'(){
+		$("#imgTitle").val('');
+		$("#imgPath").val('');
+		$("#imgDesc").val('');
+		$("#addImgPreview").attr('src','nba.jpg');
 		$("#addImgModal").modal("hide");
 	},
-	'change #imgPath'(event){
+	'input #imgPath'(event){
 		var imgPath = $("#imgPath").val();
 		$("#addImgPreview").attr('src', imgPath);
 	}
+});
 
+Template.mainBody.helpers({
+	imagesFound(){
+		return imagesDB.find().count();
+	},
+	allImages(){
+		return imagesDB.find();
+	}
+});
+
+Template.mainBody.events({
+	'click .js-deleteImg'(){
+		var imgId = this._id;
+		$("#"+imgId).fadeOut('slow', function(){
+			imagesDB.remove({_id:imgId});
+		});
+	},
+	'click .js-editImage'(){
+		var imgId = this._id;
+		$('#ImgPreview').attr('src',imagesDB.findOne({_id:imgId}).path);
+		$("#eimgTitle").val(imagesDB.findOne({_id:imgId}).title);
+		$("#eimgPath").val(imagesDB.findOne({_id:imgId}).path);
+		$("#eimgDesc").val(imagesDB.findOne({_id:imgId}).desc);
+		$('#eId').val(imagesDB.findOne({_id:imgId})._id);
+		$('#editImgModal').modal("show");
+	}
+});
+
+Template.editImg.events({
+	'click .js-updateImg'(){
+		var eId = $('#eId').val();
+		var imgTitle = $("#eimgTitle").val();
+		var imgPath = $("#eimgPath").val();
+		var imgDesc = $("#eimgDesc").val();
+		imagesDB.update({_id:eId}, {$set:{"title":imgTitle, "path":imgPath, "desc":imgDesc}});
+		$('#editImgModal').modal("hide");
+	}
 });
