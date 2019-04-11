7916f2f324b82b0b90c37809fc3c47e13c8363cc
nbayoungnate4kt <nathanael134@hotmail.com>
2019-04-11 16:09:44 -0300

    add scroll

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session'
 
 import './main.html';
 import '../lib/collection.js';
+
+Session.set('imgLimit', 3);
 
 Template.myJumbo.events({
 	'click .js-addImg'(event){
@@ -21,7 +24,7 @@ Template.addImg.events({
 		$("#imgDesc").val('');
 		$("#addImgPreview").attr('src','nba.jpg');
 		$("#addImgModal").modal("hide");
-		imagesDB.insert({"title":imgTitle, "path":imgPath, "desc":imgDesc, "createdOn":Date()});
+		imagesDB.insert({"title":imgTitle, "path":imgPath, "desc":imgDesc, "createdOn":new Date().getTime()});
 	},
 	'click .js-cancelAdd'(){
 		$("#imgTitle").val('');
@@ -40,27 +43,42 @@ Template.mainBody.helpers({
 	imagesFound(){
 		return imagesDB.find().count();
 	},
-	imageAge(){
-		var imgCreatedOn = imagesDB.findOne({_id:this._id}).createdOn;
-		imgCreatedOn = Math.round((new Date() - imgCreatedOn)/60000);
-		var timeunit = "mins";
+	imageAge(){		
+		var imgCreatedOn = imagesDB.findOne({_id:this._id}).createdOn;		
+		//convert to mins
+		imgCreatedOn = Math.round((new Date() - imgCreatedOn)/60000);		
+		var timeUnit = " mins";
+		//greater than 60 mins then convert to hours
 		if (imgCreatedOn > 60){
-			imgCreatedOn=Math.round(imgCreatedOn/60);	
-			timeunit = "hours";		
+			imgCreatedOn=Math.round(imgCreatedOn/60);
+			//hour or hours
+			if (imgCreatedOn > 1){
+				timeUnit = " hours";
+			} else {
+				timeUnit = " hour";
+			}
 		} else if (imgCreatedOn > 1440){
 			imgCreatedOn=Math.round(imgCreatedOn/1440);
-			timeunit = "days";			
+			if (imgCreatedOn > 1){
+				timeUnit = " days";
+			} else {
+				timeUnit = " day";
+			}
 		}
-		return imgCreatedOn + timeunit;
-
+		return imgCreatedOn + timeUnit;
 	},
 	allImages(){
-		var preTime = new Date() - 15000;
-		var newResults = imagesDB.find({"createdOn":{$gte:prev}})
-		if (newResults > 0){
-			return imagesDB.find
+		//Get time 15 seconds ago
+		var prevTime = new Date() - 15000;
+		var newResults = imagesDB.find({"createdOn":{$gte:prevTime}}).count();
+		if (newResults > 0) {
+			//if new images are found then sort by date first then ratings
+			return imagesDB.find({}, {sort:{createdOn:-1, imgRate:-1}, limit:Session.get('imgLimit')});
+		} else {
+			//else sort by ratings then date
+			return imagesDB.find({}, {sort:{imgRate:-1, createdOn:1}, limit:Session.get('imgLimit')});
 		}
-		return imgCreatedOn;
+		
 	}
 });
 
@@ -83,7 +101,7 @@ Template.mainBody.events({
 	'click .js-rate'(event){
 		var imgId = this.data_id;
 		var rating = $(event.currentTarget).data('userrating');
-		imagesDB.update({_id:imgId}, {$set:{'imgRate':rating}})
+		imagesDB.update({_id:imgId}, {$set:{'imgRate':rating}});
 	}
 });
 
@@ -95,5 +113,18 @@ Template.editImg.events({
 		var imgDesc = $("#eimgDesc").val();
 		imagesDB.update({_id:eId}, {$set:{"title":imgTitle, "path":imgPath, "desc":imgDesc}});
 		$('#editImgModal').modal("hide");
+	}
+});
+
+lastScrollTop = 0;
+$(window).scroll(function(event){
+	if ($(window).scrollTop() + $(window). height() > $(document).height() - 100) {
+		var scrollTop = $(this).scrollTop();
+		if (scrollTop > lastScrollTop){
+			console.log("we have arrived at the bottom of the page");
+			Session.set('imgLimit', Session.get('imgLimit') + 3);
+		}
+		lastScrollTop = scrollTop;
+		
 	}
 });
